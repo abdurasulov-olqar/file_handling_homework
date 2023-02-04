02cec51d08a653ffde7b25b95479d93aecfa8b05
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-04 15:31:43 +0500

    non-digits

--- file04.py
+++ file04.py
@@ -6,5 +6,10 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    lis = []
+    for i in data:
+        if i not in ('0123456789'):
+            lis.append(i)
+
+    return lis
 # Read data from file
